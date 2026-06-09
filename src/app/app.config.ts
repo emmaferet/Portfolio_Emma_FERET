4264061f2ffd8115862027efcbe6256c73b87434
Emma Feret <emma.feret@wanadoo.fr>
2026-06-09 14:53:14 +0200

    feat: angular 22 ng update @angular/material@22 and fix ts config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import {
   provideClientHydration,
   withEventReplay,
@@ -14,6 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay(), withNoIncrementalHydration()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
   ],
 };
