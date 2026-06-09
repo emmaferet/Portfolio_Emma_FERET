42499efe4d0b0c001d1b5985870461f81268e400
Emma Feret <emma.feret@wanadoo.fr>
2026-06-09 14:19:09 +0200

    feat: :sparkles: page my story update structure

--- src/app/features/my-story-page/my-story-page.ts
+++ src/app/features/my-story-page/my-story-page.ts
@@ -1,21 +1,29 @@
 import { Component, computed, inject, ChangeDetectionStrategy } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { Biography } from '@shared/components/biography/biography';
 import { ExperienceArticle } from '@shared/components/experience-article/experience-article';
 import { MyStoryFormations } from '@shared/components/my-story-formations/my-story-formations';
 import { FormationService } from '@shared/services/formation/formation.service';
+import { UserService } from '@shared/services/user/user.service';
 import { ExperienceService } from './../../shared/services/experience/experience.service';
 
 @Component({
   selector: 'app-my-story-page',
-  imports: [MyStoryFormations, ExperienceArticle],
+  imports: [MyStoryFormations, ExperienceArticle, Biography],
   templateUrl: './my-story-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './my-story-page.scss',
 })
 export class MyStoryPage {
-  private userService = inject(FormationService);
+  private userService = inject(UserService);
 
-  userFormations = toSignal(this.userService.getFormations(), {
+  userInfos = toSignal(this.userService.getUser(), {
+    initialValue: undefined,
+  });
+
+  private formationService = inject(FormationService);
+
+  userFormations = toSignal(this.formationService.getFormations(), {
     initialValue: undefined,
   });
 
@@ -24,7 +32,5 @@ export class MyStoryPage {
   userExperiences = toSignal(this.experienceService.getExperiences(), {
     initialValue: undefined,
   });
-  reversedExperiencesList = computed(() => {
-    return this.userExperiences()?.reverse();
-  });
+  reversedExperiencesList = computed(() => this.userExperiences()?.slice().reverse() ?? []);
 }
