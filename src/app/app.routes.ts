14fc59645efa4c7bfff7b5e9d2640ee438486767
Emma Feret <emma.feret@wanadoo.fr>
2026-08-19 16:01:41 +0200

    docs: :memo: cv update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePage } from './features/home-page/home-page';
 
 export const routes: Routes = [
-  { path: '', component: HomePage, title: 'My Home Page' },
+  { path: '', component: HomePage, title: 'Home Page' },
   // Déclarer une route classique, elle est chargée directement au lancement de l'app !!
   // { path: 'projects', component: ProjectsPage, title: 'My Projects Page', }
   {
